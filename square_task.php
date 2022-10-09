26304fbf20442942cc9eaf7465af39c1f633e372
kazhesman <kazhesman@gmail.com>
2022-10-09 18:13:48 +0300

    add a == 0 way

--- square_task.php
+++ square_task.php
@@ -4,11 +4,17 @@
 $a = rand(-10, 10);
 $b = rand(-10, 10);
 $c = rand(-10, 10);
-$d = $b * $b - 4 * $a * $c;
+if($a == 0 and $b != 0) {
+    $x = -$c/$b;
+    $d = null;
+}
+else {
+    $d = $b * $b - 4 * $a * $c;
+}
 if ($d < 0) {
     echo "<span style= \" color: gray;\">квадратное уравнение не имеет корней</span>";
 }
-if ($d == 0) {
+if ($d === 0) {
     $x = -$b / 2 * $a;
     echo "<span style= \" color: green;\">Корень уравнения:</span>" . $x;
 }
@@ -17,6 +23,6 @@ if ($d > 0) {
     $x2 = (-$b - sqrt($d)) / 2 * $a;
     echo "<span style= \" color: blue;\">Корень уравнения:</span>" . $x1 . " или " . $x2;
 }
-// else {
-//     echo "<span style= \" color: red;\">Ошибка</span>";
-// }
+ if($d === null) {
+     echo "<span style= \" color: purple;\">Это не квадратное уровнение, но корень $x</span>";
+ }
